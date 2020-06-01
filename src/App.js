8383546f1767d709a74380cacf8c65864cacbc19
yhussin <yhussin@gmail.com>
2020-05-31 19:54:05 -0700

    Working google map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-// import Map from './Map'
-import Quakes from './components/Quakes'
+//import Map from './components/Map';
+import Quakes from './components/Quakes';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+const style = {
+  width: '40%',
+  height: '80%'
+}
+
 class App extends Component {
     state = {
       earthquakes: []
     }
-
 
     componentDidMount() {
       fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson')
@@ -23,8 +29,31 @@ class App extends Component {
     return (
       <div className="app">
         <div className="mapContainer">
-          ...put Map Component here...
+       
+          
+        <Map google={this.props.google} 
+        zoom={14}
+        style={style}
+        initialCenter={{
+          lat: 37.78,
+          lng: -122.44
+        }}
+        >
+            <Marker
+            name={'Dolores park'}
+            position={{lat: 37.759703, lng: -122.428093}} />
+                <Marker onClick={this.onMarkerClick}
+                 name={'Current location'}
+                />
+
+            <InfoWindow onClose={this.onInfoWindowClose}>
+                <div>
+                  <h1>{this.state.selectedPlace}</h1>
+                </div>
+            </InfoWindow>
+            </Map>
         </div>
+
         <div className="quakeContainer">
           <h1>Earthquakes from the past month: </h1>
           < Quakes result={this.state.earthquakes} />
@@ -34,4 +63,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg')
+})(App)
--- src/components/Quakes.js
+++ src/components/Quakes.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-	let Quakes = (props) => {
-		return (
-			<div>
-				{props.result.map((item, i) => {
-					console.log(this.props)
-					return <p key={i}>{ item.properties.title }</p>			
-				})}
-			</div>
-		)
-	}
+let Quakes = (props) => {
+    return (
+        <div>
+            {props.result.map((item, i) => {
+                console.log(this.props)
+                return <p key={i}>{item.properties.title}</p>
+            })}
+        </div>
+    )
+};
+
 export default Quakes;
