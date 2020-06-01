f2a62ddbf808f218c0523fa6e42b4fe0f0276444
yhussin <yhussin@gmail.com>
2020-06-01 08:40:01 -0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-//import Map from './components/Map';
+import MapContainer from './components/Map';
 import Quakes from './components/Quakes';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const style = {
-  width: '40%',
-  height: '80%'
-}
+
 
 class App extends Component {
     state = {
@@ -31,27 +28,7 @@ class App extends Component {
         <div className="mapContainer">
        
           
-        <Map google={this.props.google} 
-        zoom={14}
-        style={style}
-        initialCenter={{
-          lat: 37.78,
-          lng: -122.44
-        }}
-        >
-            <Marker
-            name={'Dolores park'}
-            position={{lat: 37.759703, lng: -122.428093}} />
-                <Marker onClick={this.onMarkerClick}
-                 name={'Current location'}
-                />
-
-            <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                  <h1>{this.state.selectedPlace}</h1>
-                </div>
-            </InfoWindow>
-            </Map>
+        <MapContainer detail={this.state.earthquakes} />
         </div>
 
         <div className="quakeContainer">
@@ -63,6 +40,4 @@ class App extends Component {
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg')
-})(App)
+export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,61 @@
 import React, { Component } from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+
+const style = {
+    width: '40%',
+    height: '80%'
+  }
+
+class MapContainer extends Component {
+
+    displayMarkers = () => {
+        return this.props.detail.map((location, index) => {
+            return (
+                <Marker
+                    key={index}
+                    id={index}
+                    position={{
+                        lat: location.geometry.coordinates[1],
+                        lng: location.geometry.coordinates[0]
+                    }}
+                />
+            )
+        })
+    }
+
+
+    render() {
+        return (
+
+            <Map google={this.props.google}
+                zoom={14}
+                style={style}
+                initialCenter={{
+                    lat: 37.78,
+                    lng: -122.44
+                }}
+            >
+            {this.displayMarkers()}
+            </Map>
 
 
 
-export default Map;
+
+
+            // <Map className='the-map'
+            // google={this.props.google}
+            // zoom={4}
+            // style={{ width: '100%' }}
+            // containerStyle={{ width: '40%' }}
+            // center={{ lat: 37.78, lng: -122.44 }}
+            // >
+            // {this.displayMarkers()}
+            // </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg')
+  })(MapContainer)
